a0d183be3a5e1c5ab811e86376e0e4c315bc9c56
anandk2662 <130994394+anandk2662@users.noreply.github.com>
2025-10-01 18:56:55 +0530

    uploading calculator project

---
+++ calculator/script.js
@@ -0,0 +1,40 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      try{
+      string = eval(string);
+      document.querySelector("input").value = string;
+      }
+      catch(erroe){
+        document.querySelector("input").value = 'error';
+      }
+      
+    } else if (e.target.innerHTML == "AC") {
+      try{string = "";
+      document.querySelector("input").value = string;
+      }
+      catch(erroe){
+        document.querySelector("input").value = 'error';
+      }
+    } else if (e.target.innerHTML == "DEL") {
+      try{string = string.slice(0, -1);
+      document.querySelector("input").value = string;
+      }
+      catch(erroe){
+        document.querySelector("input").value = 'error';
+      }
+    } else if (e.target.innerHTML == "%") {
+      try {
+        string = (eval(string) / 100).toString();
+      } catch {
+        string = "Error";
+      }
+      document.querySelector("input").value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
